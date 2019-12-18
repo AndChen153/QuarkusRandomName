4650a7b375f0bc021b692c5af6f8dd1b1b40f823
AndChen153 <achen1192@gmail.com>
2019-12-17 21:17:37 -0600

    added java random name function

--- randommane-quarkus/src/main/java/com/sample/QuarkusEndpoint.java
+++ randommane-quarkus/src/main/java/com/sample/QuarkusEndpoint.java
@@ -11,6 +11,11 @@ public class QuarkusEndpoint {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "hello";
+        final String[] names = { "Liam", "Noah", "William", "James", "Oliver", "Benjamin", "Elijah", "Lucas", "Mason",
+                "Logan", "Alexander", "Ethan", "Emma", "Emma", "Olivia", "Ava", "Isabella", "Sophia", "Charlotte",
+                "Mia", "Amelia", "Harper", "Evelyn", "Abigail", "Emily" };
+        final int nameIndex = (int) (names.length * Math.random());
+        return names[nameIndex];
+
     }
 }
